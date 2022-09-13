539d7fa5731d0710414e033f2c934fe9a9e0f089
alan <cvinny33@gmail.com>
2022-09-12 18:46:20 -0600

    added comments to the server.js file

--- server.js
+++ server.js
@@ -1,25 +1,30 @@
+// Dependencies
 const express = require('express');
 const fs = require('fs');
 const {writeFile, readFile} = fs.promises
 const {v4: uuidv4} = require('uuid')
-const PORT = process.env.PORT || 3001;
 const path = require('path')
 
+// Sets up the Express App
 const app = express();
+const PORT = process.env.PORT || 3001;
 
+// Sets up the routes
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'))
 
-
+// Gets the route index.html
 app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
+// Gets the route notes.html
 app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+// Reads the db.json file and returns all saved notes as JSON
 app.get('/api/notes', async (req, res) => {
 try {
     const readDb = await readFile('db/db.json')
@@ -30,6 +35,7 @@ try {
 }
 })
 
+// Receives a new note to save and adds it to the db.json file. gives each note a unique id.
 app.post('/api/notes', async (req, res) => {
 try {
     const readDb = await readFile('db/db.json')
@@ -42,6 +48,8 @@ try {
     res.status(500).json(error.message)
 }
 })
+
+// Starts the server to begin listening
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
 );
